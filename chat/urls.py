35c2267ed856a1e987a51c2eebe1bf389c054f74
Alec Kienzle <alec.kienzle@gmail.com>
2021-06-07 16:34:57 -0700

    loading last message after each interval

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.load_messages, name='load_messages'),
-	path('', views.create_message, name='create_message')
+	path('', views.create_and_load_messages, name='create_and_load_messages'),
+	#path('', views.create_message, name='create_message')
 ]
--- chat/views.py
+++ chat/views.py
@@ -10,32 +10,26 @@ from .models import Message
 from django.contrib.auth.models import User
 from .serializers import MessageSerializer
 
-#class ChatView(generics.CreateAPIView):
-#	queryset = Message.objects.all()
-#	serializer_class = MessageSerializer
-
-
-#list all messages, or create a new message	
 
 def latest_message(request):
     return Message.objects.latest("timestamp").timestamp
 
 @last_modified(latest_message)
-def load_messages(request):
+def create_and_load_messages(request):
 	if request.method == 'GET':
-		messages = Message.objects.all()
+		#messages = Message.objects.all().order_by('-id')[:1]
+		messages = Messages.objects.all()
 		serializer = MessageSerializer(messages, many=True)
 		response = JsonResponse(serializer.data, safe=False)
 		response["Access-Control-Allow-Origin"] = "*"
 		return response	
-
-def create_message(request):
-	if request.method == 'POST':
+	elif request.method == 'POST':
 		serializer = MessageSerializer(data=request.POST)
 		if serializer.is_valid():
 			#Message.objects.create()
 			serializer.save()
 			return HttpResponse(serializer.data, status=201)
-		return HttpResponse(serializer.errors, status=400)						
+		return HttpResponse(serializer.errors, status=400)	
+					
 
 #request.GET.get('param1', None)
