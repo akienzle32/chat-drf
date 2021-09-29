6eeefb358edc16891997e5f7ba80b511b6c30c0d
Alec Kienzle <alec.kienzle@gmail.com>
2021-09-29 10:56:55 -0700

    changed default value in Message model to global constant for ease of understanding

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+DEFAULT_CHAT_PK = 1;
+
 class Chat(models.Model):
 	name = models.CharField(max_length=50)
 
@@ -14,6 +16,6 @@ class Message(models.Model):
 	author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	content = models.TextField()
-	chat = models.ForeignKey(Chat, on_delete=models.CASCADE, default=1)
+	chat = models.ForeignKey(Chat, on_delete=models.CASCADE, default=DEFAULT_CHAT_PK)
 
 # Create your models here. 
