a7f8276c8cf71e24217c46388e9cb32c9f1532c1
Alec Kienzle <alec.kienzle@gmail.com>
2021-10-14 11:15:40 -0700

    changed allowed hosts setting

--- chat_api/settings.py
+++ chat_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['alec-chat-api.herokuapp.com']
 
 
 # Application definition
