abca9b70928d1c4f79ad00c00451914a2141cb77
Alec Kienzle <alec.kienzle@gmail.com>
2021-10-16 13:40:43 -0700

    added AllowAny import to views

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from rest_framework.request import Request
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from email.utils import parsedate_to_datetime
 from datetime import datetime, timedelta
 
